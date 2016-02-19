6d0d97bddef6a16206c9bca8f3425cf898fa7137
bcamilly <Brian@Brians-iMac.local>
2016-02-18 20:35:44 -0500

    Initial Commit
    Basic API functionality

---
+++ OandA_API.cpp
@@ -0,0 +1,428 @@
+#include "OandA_API.hpp"
+
+
+OA::OA_API::OA_API()
+{
+    RestClient::init();
+    restConn = new RestClient::Connection("https://api-fxpractice.oanda.com");
+
+
+}
+
+void OA::OA_API::useAccount(string localAccount)
+{
+    account = localAccount;
+}
+
+void OA::OA_API::useInstrument(string localInstrument)
+{
+    instrument = localInstrument;
+}
+
+bool OA::OA_API::refreshOrders()
+{
+    //bool returnValue = false;
+    vector<OA::Order> localOrders;
+    
+    Json::Value currentOrders;
+    Json::Reader reader;
+
+    if (account == "")
+    {
+        return false;
+    }
+
+    string url = "/v1/accounts/" + account + "/orders";
+
+    RestClient::HeaderFields headers;
+    headers["Authorization"] = "Bearer " + authCode;
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+    restConn->SetHeaders(headers);
+    RestClient::Response r = restConn->get(url);
+
+    reader.parse(r.body, currentOrders);
+
+    cout << "Current Orders:--------------------------------------" << endl << currentOrders << endl;
+
+    for (Json::ValueIterator itr = currentOrders["orders"].begin() ; itr != currentOrders["orders"].end() ; itr++)
+    {
+        Json::Value singleOrderJSON = *itr;
+        //cout << "This is the single orders: " << endl << singleOrderJSON << endl;
+
+        OA::Order singleOrder;
+        singleOrder.id = singleOrderJSON["id"].asInt64();
+        singleOrder.symbol = singleOrderJSON["instrument"].asString();
+        singleOrder.units = singleOrderJSON["units"].asDouble();
+
+        localOrders.push_back(singleOrder);
+    }
+
+    cout << "There are " << localOrders.size() << " orders in the vector" << endl;
+
+    orders = localOrders;
+
+    return true;
+}
+
+bool OA::OA_API::refreshTrades()
+{
+    //bool returnValue = false;
+    vector<OA::Trade> localTrades;
+
+    Json::Value currentTrades;
+    Json::Reader reader;
+
+    if (account == "")
+    {
+        return false;
+    }
+
+    string url = "/v1/accounts/" + account + "/trades";
+
+    RestClient::HeaderFields headers;
+    headers["Authorization"] = "Bearer " + authCode;
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+    restConn->SetHeaders(headers);
+    RestClient::Response r = restConn->get(url);
+    //RestClient::response r = RestClient::get(url, headers);
+
+    reader.parse(r.body, currentTrades);
+    cout << "Current Trades: -----------------------------------------------" << endl << currentTrades << endl;
+
+    for (Json::ValueIterator itr = currentTrades["trades"].begin() ; itr != currentTrades["trades"].end() ; itr++)
+    {
+        Json::Value singleTradeJSON = *itr;
+        //cout << "This is the single orders: " << endl << singleTradeJSON << endl;
+
+        OA::Trade singleTrade;
+        singleTrade.id = singleTradeJSON["id"].asInt64();
+        singleTrade.symbol = singleTradeJSON["instrument"].asString();
+        singleTrade.units = singleTradeJSON["units"].asDouble();
+
+        localTrades.push_back(singleTrade);
+    }
+
+    cout << "There are " << localTrades.size() << " trades in the vector" << endl;
+
+    trades = localTrades;
+
+    return true;
+
+}
+
+bool OA::OA_API::refreshPositions()
+{
+
+    return false;
+}
+
+
+
+bool OA::OA_API::createOrder(string instrument, int units, string side, string type, string ex, double price, double takeProfit)
+{
+    //boost::posix_time::ptime time_now(second_clock::local_time());
+    if (account.length() == 0)
+    {
+        return false;
+    }
+    bool returnValue;
+    string url = "https://api-fxpractice.oanda.com/v1/accounts/" + account + "/orders/";
+    string data = "instrument=" + instrument;
+    data += "&units=" + to_string(units);
+    data += "&side=" + side;
+    data += "&type=" + type;
+    data += "&expiry=" + ex;
+    data += "2016-04-01T00%3A00%3A00Z";
+    data += "&price=" + to_string(price);
+    data += "&takeProfit=" + to_string(takeProfit);
+    
+    RestClient::HeaderFields headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+    restConn->SetHeaders(headers);
+    //cout << "Data: " << data << endl << endl;
+
+    RestClient::Response r = restConn->post(url, data);
+
+    cout << "Return code: " << r.code << ", " << r.body << endl;
+
+    if (r.code == 201)
+    {
+        returnValue = true;
+    }
+    else
+    {
+        returnValue = false;
+    }
+
+    if (returnValue)
+    {
+        refreshOrders();    
+    }
+        
+    return returnValue;
+}
+
+bool OA::OA_API::deleteOrder(unsigned long long int id)
+{
+    bool returnValue = false;
+    if (account.length() == 0)
+    {
+        return false;
+    }
+
+    string url = "/v1/accounts/" + account + "/orders/" + to_string(id);
+
+    RestClient::HeaderFields headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+    restConn->SetHeaders(headers);
+
+    RestClient::Response r = restConn->del(url);
+    
+    
+    if (r.code == 200)
+    {
+        returnValue = true;
+        //cout << "Order " << to_string(id) <<  " successfully deleted" << endl;
+    }
+    else
+    {
+        returnValue = false;
+        cout << "There was a problem deleting the order: " << r.code << endl;
+        cout << "Response body: " << r.body << endl;
+    }
+    
+    if (returnValue)
+    {
+        refreshOrders();    
+    }
+
+    return returnValue;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Json::Value getInstrument(string theInstrument, string account)
+{
+    Json::Value instrumentList;
+    Json::Reader reader;
+
+    string url = "https://api-fxpractice.oanda.com/v1/instruments?accountId=" + account + "&instruments=" + theInstrument;
+
+    RestClient::headermap headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+    RestClient::response r = RestClient::get(url, headers);
+    
+    bool success = reader.parse(r.body, instrumentList);
+    if (!success)
+    {
+        cout << "There was an error parsing" << endl;
+    }   
+    
+    //cout << "getInstrumentList return value: " << instrumentList << endl;
+
+    return instrumentList;
+}
+*/
+/*
+Json::Value getCurrentPrices(string theInstrument)
+{
+    Json::Value currentPrices;
+    Json::Reader reader;
+
+    string url = "https://api-fxpractice.oanda.com/v1/prices?instruments=" + theInstrument;
+
+    RestClient::headermap headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+    RestClient::response r = RestClient::get(url, headers);
+    
+    bool success = reader.parse(r.body, currentPrices);
+    cout << "Success? " << success << endl;
+    
+    //cout << "getCurrentPrices return value: " << currentPrices["prices"] << endl;
+
+    return currentPrices["prices"];
+
+}
+*/
+/*
+Json::Value getCurrentOrders(string account)
+{
+
+    Json::Value currentOrders;
+    Json::Reader reader;
+
+    string url = "https://api-fxpractice.oanda.com/v1/accounts/" + account + "/orders";
+
+    RestClient::headermap headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+    RestClient::response r = RestClient::get(url, headers);
+
+    reader.parse(r.body, currentOrders);
+    //cout << "Success? " << success << endl;
+    
+    //cout << "getCurrentOrders return value: " << currentOrders << endl;
+
+    return currentOrders["orders"];
+
+}
+*/
+/*
+Json::Value getOpenTrades(string account)
+{
+
+    Json::Value currentTrades;
+    Json::Reader reader;
+
+    string url = "https://api-fxpractice.oanda.com/v1/accounts/" + account + "/trades";
+
+    RestClient::headermap headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+    RestClient::response r = RestClient::get(url, headers);
+    
+    reader.parse(r.body, currentTrades);
+    //cout << "Success? " << success << endl;
+    
+    //cout << "getOpenTrades return value: " << currentTrades << endl;
+
+    return currentTrades["trades"];
+
+}
+*/
+/*
+
+Json::Value getHistory(string instrument, string granularity, boost::posix_time::ptime start, boost::posix_time::ptime end, int count)
+{
+    RestClient::headermap headers;
+    headers["Authorization"] = "Bearer ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+    headers["X-Accept-Datetime-Format"] = "RFC3339";
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+    
+    Json::Value historyBars;
+    Json::Reader reader;
+    string url = "https://api-fxpractice.oanda.com/v1/candles?instrument=" + instrument + "&granularity=" + granularity + "&candleFormat=midpoint";
+
+    cout << "Testing start and end: " << boost::posix_time::to_iso_extended_string(start) << ", End: " << boost::posix_time::to_iso_extended_string(end) << endl;
+
+
+
+    if (!start.is_not_a_date_time() && !end.is_not_a_date_time())
+    {
+        cout << "Getting history for Start: " << boost::posix_time::to_iso_extended_string(start) << ", End: " << boost::posix_time::to_iso_extended_string(end) << endl;
+        
+        string startString = urlEncode(to_iso_extended_string(start) + "Z" );
+        string endString = urlEncode(to_iso_extended_string(end) + "Z" );
+        
+        url += "&start=";
+        url += startString;
+        
+        url += "&end=";
+        url += endString;
+        
+        cout << "URL: " << url << endl;
+    }
+    else if (!start.is_not_a_date_time() && end.is_not_a_date_time() && count != 0)
+    {
+        string startString = urlEncode(to_iso_extended_string(start) + "Z" );
+        
+        url += "&start=";
+        url += startString;
+        
+        url += "&count=";
+        url += to_string(count);
+        
+        cout << "URL: " << url << endl;
+    }
+    else if (start.is_not_a_date_time() && !end.is_not_a_date_time() && count != 0)
+    {
+        string endString = urlEncode(to_iso_extended_string(end) + "Z" );
+        
+        url += "&end=";
+        url += endString;
+        
+        url += "&count=";
+        url += to_string(count);
+        
+        cout << "URL: " << url << endl;
+    }
+    else if (count != 0)
+    {
+        url += "&count=";
+        url += to_string(count);
+    
+    
+    }
+
+    
+
+    //cout << "This is the history URL: " << url << endl;
+
+    RestClient::response r = RestClient::get(url, headers);
+    if (r.code != 200)
+    {
+        cout << r.body << endl;
+    }
+        
+    reader.parse(r.body, historyBars);
+    //cout << "Success? " << success << endl;
+    
+    //cout << "getHistory return value: " << historyBars << endl;
+
+    return historyBars;
+    
+}
+
+*/
+
+
+
+
+
+
+
+
+
---
+++ OandA_API.hpp
@@ -0,0 +1,145 @@
+#ifndef _OandA_API_h
+#define _OandA_API_h
+
+#include <sstream>
+#include <string>
+
+//#include <curlpp/cURLpp.hpp>
+//#include <curlpp/Easy.hpp>
+//#include <curlpp/Options.hpp>
+
+#include <json/json.h>
+#include <restclient-cpp/connection.h>
+//#include <restclient-cpp/restclient.h>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#include "functions.hpp"
+
+using namespace std;
+
+namespace OA
+{
+    
+    struct Order
+    {
+        string id;
+        string symbol;
+        int units;
+        string direction;
+        string type;
+        string timeStamp;
+        double price;
+        double takeProfit;
+        double stopLoss;
+        string expiry;
+        double upperBound;
+        double lowerBound;
+        double trailingStop;        
+    };
+        
+    struct Trade
+    {
+        string id;
+        string symbol;
+        int units;
+        string direction;
+        string timeStamp;
+        double price;
+        double takeProfit;
+        double stopLoss;
+        
+        
+        
+    };
+        
+    struct Position
+    {
+        
+        
+    };
+    
+    struct Bar
+    {
+        double open;
+        double close;
+        double high;
+        double low;
+        string timeStamp;
+         
+        bool operator == (const string& test) const
+        {
+            return  test == timeStamp;
+        }
+            
+        Bar(double tempOpen, double tempClose, double tempHigh, double tempLow, string tempTimeStamp)
+        {
+            open = tempOpen;
+            close = tempClose;
+            high = tempHigh;
+            low = tempLow;
+            timeStamp = tempTimeStamp;
+        }     
+    };
+
+    class OA_API;
+        
+}
+
+class OA::OA_API
+{
+
+    public:
+    
+        OA_API();
+        ~OA_API();
+    
+        void useAccount(string localAccount);
+        void useInstrument(string localInstrument);
+        
+        bool createOrder(string instrument, int units, string side, string type, string ex, double price, double takeProfit);
+        bool getOrderInfo(string id);
+        bool modifyOrder(string id);
+        bool deleteOrder(unsigned long long id);
+        
+        bool refreshOrders();
+        bool refreshTrades();
+        bool refreshPositions();
+        
+    
+    
+
+        vector<OA::Order> orders;
+        vector<OA::Trade> trades;
+        vector<OA::Position> positions;
+    
+    
+    private:
+    
+        RestClient::Connection *restConn;
+        string account = "";
+        string instrument = "";
+        string authCode = "ca2624a2fcb1374847a802ed7bb38295-813cd7c17bdf044a90ebcd274b1414c4";
+        
+        
+        //OA::Order createOrder_JSON(Json::Value);
+
+
+};
+
+
+
+
+
+
+//Json::Value getInstrument(string theInstrument, string account);
+//Json::Value getCurrentPrices(string theInstrument);
+//Json::Value getCurrentOrders(string account);
+//Json::Value getOpenTrades(string account);
+//Json::Value getHistory(string instrument, string granularity, boost::posix_time::ptime start, boost::posix_time::ptime end, int count = 0);
+//bool deleteOrder(unsigned long long int id, string account);
+
+//bool createOrder(string instrument, int units, string side, string type, string ex, double price, double takeProfit, string account);
+
+
+
+#endif
